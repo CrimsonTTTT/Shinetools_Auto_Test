59cc148a6e930334da497fbaeabbbc371b2c559a
CrimsonTTTT <Graycat_163@163.com>
2023-11-22 17:51:23 +0800

    测试用

--- Shinetools-Auto-Test/src/main/java/testcase/XXXCase.java
+++ Shinetools-Auto-Test/src/main/java/testcase/XXXCase.java
@@ -21,7 +21,7 @@ public class XXXCase {
     String excelPath = "D:\\TestCode\\ExcelTest.xlsx";
 
     /**
-     * Description:  调试用，用来测试部分代码；测试远程代码冲突
+     * Description:  调试用，用来测试部分代码；本地代码分支修改
      * @param
      * @return void
      * @author Graycat. 2023/10/24 14:58
